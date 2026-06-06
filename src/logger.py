96547cdc6daea629a8b9cbf123c38aadc21789d3
Adrijo Ray <adrijoray@gmail.com>
2026-06-06 11:47:51 +0530

    edited the Logger file

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 log_file = f'{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log'
-log_path = os.path.join(os.getcwd(),'logs')
+log_path = os.path.join(os.getcwd(),'logs',log_file)
 os.makedirs(log_path,exist_ok=True)
 
 log_file_path = os.path.join(log_path,log_file)
