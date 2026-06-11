732a391e397549542d351245a081fb8b36b41e24
Adrijo Ray <adrijoray@gmail.com>
2026-06-11 16:26:22 +0530

    created Prediction pipeline and app.py

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import pickle
 from src.exception import CustomException
 from src.logger import logging
+import dill
 from sklearn.metrics import r2_score, mean_squared_error
 
 def save_object(file_path, obj):
@@ -34,3 +35,11 @@ def evaluate_models(X_train,X_test,y_train,y_test,models):
 
     except Exception as e:
         raise CustomException(e,sys)        
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    
+    except Exception as e:
+        raise CustomException(e,sys)
